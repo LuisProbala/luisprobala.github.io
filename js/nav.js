d5d4e09d982bc9180e0b340112a92186030a8c80
luisprobala <luis.probala@gmail.com>
2019-11-21 14:10:09 -0300

    arbo images ++

--- js/nav.js
+++ js/nav.js
@@ -1,23 +1,5 @@
-/*()
-$(".nav-item").on("click", function() {
-        var $this = $(this);
-        var $target = $(this).parent().find(".dropdown-inside").first();
-        if ($target.length != 0) {
-            if (!$target.is(":visible"))
-                $this.find("i").addClass("rotate");
-            else
-                $this.find("i").removeClass("rotate");
-            $target.toggle(400)
-        }
-    });
-
-    function changeSize(id) {
-        var el = document.getElementByClassName(id);
-        if (el) {
-            el.style.height = "200px";
-            el.style.width = "200px";
-        }
-    }
+/*
+Open navigation lines, one for each
 */
 
 function openFunction() {
@@ -38,7 +20,6 @@ else {
     element.className = classes.join(" ");
 }
 }
-
 
 
 function open2Function() {
@@ -81,69 +62,7 @@ else {
 }
 }
 
-
-
-
-var elements = document.getElementsByClassName("nav-item");
-       for(var i = 0; i < elements.length; i++)
-       {
-           elements[i].onclick = function(){
-
-               // remove class from sibling
-
-               var el = elements[0];
-               while(el)
-               {
-                   if(el.tagName === "DIV"){
-                       //remove class
-                       el.classList.remove("bak");
-
-                   }
-                   // pass to the new sibling
-                   el = el.nextSibling;
-               }
-
-             this.classList.add("bak");
-           };
-       }
-
-/* word swap
-
-$(function () {
-    var hello = ['Escolas públicas.', 'Arborização urbana.', 'Siglas institucionais.', 'Espaço e linguagem.', 'Arte-educação.'];
-    var used = ['jupiter'];
-    var greeting = $('#swap');
-    var item;
-
-    function swap() {
-        item = hello[Math.floor(Math.random() * hello.length)];
-        if (hello.length != used.length) {
-            while (jQuery.inArray(item, used) != -1) {
-                item = hello[Math.floor(Math.random() * hello.length)];
-            }
-            used.push(item);
-        } else {
-            used.length = 0;
-            item = hello[Math.floor(Math.random() * hello.length)];
-            used.push(item);
-        }
-        greeting.html(item);
-        greeting.animate({
-            "opacity": "1"
-        },);
-    }
-
-    window.setInterval(function () {
-        greeting.animate({
-            "opacity": "0"
-        },);
-        setTimeout(swap, 1500);
-    }, 3000);
-
-});
-
-*/
-
+// slideshow prev
 
 $("#slideshow > div:gt(0)").hide();
 
@@ -156,6 +75,7 @@ setInterval(function() {
     .appendTo('#slideshow');
 },4000);
 
+// observ gif
 
 function hover(element) {
   element.setAttribute('src', '/img/observ2.gif');
@@ -165,7 +85,18 @@ function unhover(element) {
   element.setAttribute('src', '/img/ciep-prev1.png');
 }
 
+$("#slideshow2 > div:gt(0)").hide();
 
+setInterval(function() {
+  $('#slideshow2 > div:first')
+    .hide()
+    .next()
+    .show()
+    .end()
+    .appendTo('#slideshow2');
+},4000);
+
+// word typying
 
 
 document.addEventListener('DOMContentLoaded',function(event){
@@ -210,47 +141,3 @@ document.addEventListener('DOMContentLoaded',function(event){
   // start the text animation
   StartTextAnimation(0);
 });
-
-
-/*
-jQuery(function ($) {
-    $('.nav-item-1, .nav-item-2, .nav-item-3, .nav-item-4').hover(function () {
-        $(this).find('ul').stop(true, true).slideToggle("fast", "linear")
-    }).find('ul').hide("slow")
-})
-
-$(function () {
-        $(".subnav-item").on('click',function(){
-            $(".subnav-content-text").slideUp();
-            var divno =  $(this).attr('id');
-            $("#div_dom" + divno).slideToggle();
-        });
-    });
-
-$(function () {
-        $(".subnav-item").on('click',function(){
-            $(".subnav-content-img").slideUp();
-            var divno =  $(this).attr('id');
-            $("#div_img" + divno).slideToggle();
-        });
-    });
-
-$(function () {
-        $(".subnav-item").on('click',function(){
-            $(".material-icons").slideUp();
-            var divno =  $(this).attr('id');
-            $("#div_lnk" + divno).slideToggle();
-        });
-    });
-
-$(function () {
-        $(".subnav-item").on('click',function(){
-            $("figcaption").slideUp();
-            var divno =  $(this).attr('id');
-            $("#div_cap" + divno).slideToggle();
-        });
-    });
-
-
-
-*/
